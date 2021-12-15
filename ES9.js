5734fdfffcbf52a9d3758b0b97ba1570b1ab576f
000deepak <kumbhardeepak000@gmail.com>
2021-12-15 15:40:34 +0530

    ES9 Features

---
+++ ES9.js
@@ -0,0 +1,5 @@
+//Rest properties for object destructuring assignment.
+
+let { fname, lname, ...rest } = { fname: "Hemanth", lname: "HM", location: "Earth", type: "Human" };
+fname; //"Hemanth"
+rest; // {location: "Earth", type: "Human"}

ef4ac49ad3594b16b18bcad912058fe59da56372
000deepak <kumbhardeepak000@gmail.com>
2021-12-18 10:10:02 +0530

    ES9 Features

--- ES9.js
+++ ES9.js
@@ -1,15 +1,98 @@
 //ES9 features are
 //1.Asynchronous Iteration: 
 //2.Promise.prototype.finally:
-//3.Rest/Spread Properties
+//3.Rest/Spread(...) Properties
 
+//---------------------------------synchronoush iteration
+/* Synchronous iteration was introduced with ES6 and have the following components
+Iterable: Is an object that can be iterated via a method with key Symbol.iterator
+Iterator: An object returned by [Symbol.iterator](). It has a method .next() that returns the next element.
+NextElement: Is the object returned by .next() that has 2 properties:
+value: Contains the value to be used.
+done: Is a boolean that becomes true after the last element. */
 
+for (const coinValue of fetchCoinValues(['ETH','XMR','BTC','LTC','BAT'])) {
+  console.log(coinValue);
+}
 
 //----------------------------------------Asynchronous Iteration: 
-//EcmaScript 2018 introduced asynchronous iterators and iterables. 
-//This will help in reading text from an HTTP connection very effortlessly. 
-//A new IterationStatement known as for–await–of was introduced that also adds syntax that can help in creating async generator functions.
+// previously iterating is synchronous, it doesn’t work for asynchronous sources
+/* es9 specifies a new protocol for async iteration:
+Async iterables are marked via Symbol.asyncIterator.
+Method .next() of an async iterator returns a Promise or NextElement.
+With this new protocol, we are able to await for an asynchronous fetched coinValue */
 
+
+/* - Specifies an asynchronous version of the "for-of"loop.
+ * -"for-await-of" loop allows to iterate promises and return them on the order that they were called. 
+ */
+
+
+/**
+ * Let's imagine that we have 3 promises that they will 
+ * be resolved in different times each one
+ *  */
+ function promise1 () {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          console.log('Hello promise 1');
+          resolve(1);
+      }, 2000)
+  });
+} // resolve in 2 seconds -> third
+
+function promise2 () {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          console.log('Hello promise 2');
+          resolve(2);
+      }, 3000)
+  });
+} // resolve in 3 seconds -> fourth
+
+function promise3 () {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          console.log('Hello promise 3');
+          resolve(3);
+      }, 500)
+  });
+} // resolve in 0.5 seconds -> second
+
+/**
+* And I want to iterate all of them, 
+* but for any reason I need the results on the order that I specified.
+*/
+async function testAsynchronousIterator() {
+  const arrayPromises = [promise1(), promise2(), promise3()];
+  for await (const item of arrayPromises) {
+      console.log(item); 
+   }
+}
+
+testAsynchronousIterator();
+// 1, 2, 3, 4
+// Obs: the console.log on each promise will be showed 
+// as they are resolved, but they will be returned on the order 
+// that they was specified on the array
+//----------------------------
+
+async function* asyncGenerator() {
+  let i = 0;
+  while (i < 3) {
+    yield i++;
+  }
+}
+
+(async function() {
+  for await (let num of asyncGenerator()) {
+    console.log(num);
+  }
+})();
+// 0
+// 1
+// 2
+//------------------------------------
 
 
 //----------------------------------------Promise.prototype.finally:
@@ -21,6 +104,22 @@ let myPromise = new Promise();
 myPromise.then();
 myPromise.catch();
 myPromise.finally();
+
+//Promise API is extended by an optional finally block which is called 
+//in any case (after the Promise is resolved or is rejected).
+
+function testFinally() {
+  return new Promise((resolve,reject) => resolve())
+}
+
+testFinally().then(() => console.debug("resolved"))
+.finally(() => console.debug("finally"))
+//O/P-
+// resolved
+// finally
+
+//.finally is useful when you need to do some clean up 
+//after the operation has finished regardless  succeeded or not,for example to close a connection.
 
 
 
